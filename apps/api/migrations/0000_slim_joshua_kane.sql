76646df0e2d0e3befc89627efa3631487d9c67a9
tiesen243 <ttien56906@gmail.com>
2026-06-30 10:36:52 +0700

    feat(db): added index

--- apps/api/migrations/0000_slim_joshua_kane.sql
+++ apps/api/migrations/0000_slim_joshua_kane.sql
@@ -99,6 +99,7 @@ CREATE TABLE "subscriptions" (
 --> statement-breakpoint
 CREATE TABLE "trackings" (
 	"id" varchar(24) PRIMARY KEY NOT NULL,
+	"date" date NOT NULL,
 	"status" "tracking_statuses" DEFAULT 'PENDING' NOT NULL,
 	"taken_at" timestamp,
 	"schedule_id" varchar(24) NOT NULL
@@ -145,7 +146,19 @@ ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_device_id_devices_id_f
 ALTER TABLE "trackings" ADD CONSTRAINT "trackings_schedule_id_schedules_id_fk" FOREIGN KEY ("schedule_id") REFERENCES "public"."schedules"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "transactions" ADD CONSTRAINT "transactions_invoice_id_invoices_id_fk" FOREIGN KEY ("invoice_id") REFERENCES "public"."invoices"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "compartments_box_id_position_uq_idx" ON "compartments" USING btree ("box_id","position");--> statement-breakpoint
+CREATE INDEX "devices_user_id_idx" ON "devices" USING btree ("user_id");--> statement-breakpoint
 CREATE UNIQUE INDEX "invoices_code_uq_idx" ON "invoices" USING btree ("code");--> statement-breakpoint
+CREATE INDEX "invoices_subscription_id_idx" ON "invoices" USING btree ("subscription_id");--> statement-breakpoint
+CREATE INDEX "notifications_user_id_idx" ON "notifications" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "patients_user_id_idx" ON "patients" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "patients_device_id_idx" ON "patients" USING btree ("device_id");--> statement-breakpoint
+CREATE INDEX "schedules_patient_id_idx" ON "schedules" USING btree ("patient_id");--> statement-breakpoint
+CREATE INDEX "schedules_duration_idx" ON "schedules" USING btree ("started_at","ended_at");--> statement-breakpoint
 CREATE UNIQUE INDEX "sessions_token_uq_idx" ON "sessions" USING btree ("token");--> statement-breakpoint
+CREATE INDEX "subscriptions_next_billing_date_idx" ON "subscriptions" USING btree ("next_billing_date");--> statement-breakpoint
+CREATE INDEX "subscriptions_user_id_device_id_idx" ON "subscriptions" USING btree ("user_id","device_id");--> statement-breakpoint
+CREATE UNIQUE INDEX "trackings_schedule_id_date_uq_idx" ON "trackings" USING btree ("schedule_id","date");--> statement-breakpoint
+CREATE INDEX "trackings_date_idx" ON "trackings" USING btree ("date");--> statement-breakpoint
+CREATE INDEX "transactions_invoice_id_idx" ON "transactions" USING btree ("invoice_id");--> statement-breakpoint
 CREATE UNIQUE INDEX "users_email_uq_idx" ON "users" USING btree ("email");--> statement-breakpoint
 CREATE UNIQUE INDEX "users_username_uq_idx" ON "users" USING btree ("username");
