e21037298018808f567764934ced26ce0b70a314
tiesen243 <ttien56906@gmail.com>
2026-08-06 19:13:06 +0700

    fix(web): base url

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -2,8 +2,8 @@ import { env } from '@/lib/env'
 
 export function getBaseUrl() {
   if (typeof window !== 'undefined') return window.location.origin
-  if (env.VERCEL_PROJECT_PRODUCTION_URL)
-    return `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
-  if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`
+  if (env.VITE_VERCEL_PROJECT_PRODUCTION_URL)
+    return `https://${env.VITE_VERCEL_PROJECT_PRODUCTION_URL}`
+  if (env.VITE_VERCEL_URL) return `https://${env.VITE_VERCEL_URL}`
   return `http://localhost:${process.env.PORT ?? 5173}`
 }
